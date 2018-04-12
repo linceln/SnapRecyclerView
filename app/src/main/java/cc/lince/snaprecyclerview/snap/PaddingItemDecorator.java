9bc6e6fce9781c362f236f576415fa17d434cdfd
linceln <lince2030@gmail.com>
2018-04-12 11:40:17 +0800

    milestone1

--- app/src/main/java/cc/lince/snaprecyclerview/snap/PaddingItemDecorator.java
+++ app/src/main/java/cc/lince/snaprecyclerview/snap/PaddingItemDecorator.java
@@ -6,8 +6,8 @@ import android.support.v7.widget.RecyclerView;
 import android.view.View;
 
 public class PaddingItemDecorator extends RecyclerView.ItemDecoration {
-    private final int mSize;
     private final int mOrientation;
+    private final int mSize;
 
     public PaddingItemDecorator(int orientation, int size) {
         mSize = size;
@@ -25,7 +25,12 @@ public class PaddingItemDecorator extends RecyclerView.ItemDecoration {
                 outRect.set(0, 0, parent.getWidth() - mSize - view.getWidth() / 2, 0);
             }
         } else if (mOrientation == LinearLayoutManager.VERTICAL) {
-            outRect.set(0, 0, 0, mSize);
+            int current = parent.getChildLayoutPosition(view);
+            if (current == 0) {
+                outRect.set(0, mSize - view.getHeight() / 2, 0, 0);
+            } else if (current == state.getItemCount() - 1) {
+                outRect.set(0, 0, 0, parent.getHeight() - mSize - view.getHeight() / 2);
+            }
         }
     }
 }
